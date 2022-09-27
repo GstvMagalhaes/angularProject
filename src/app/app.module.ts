725141499d8f1ca35ce7be639e552b089bd46176
Gustavo Pereira <gustavo.magalhaes@mobilus.com.br>
2022-09-27 14:18:04 -0300

    criacao do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//PRIME NG COMPONETS
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputTextModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
